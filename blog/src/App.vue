f8b900273c49e6694c4e61090042a38a54e9258a
alvinwarui29 <alvinwarui@kabarak.ac.ke>
2023-04-08 08:20:03 +0300

    make first component

--- blog/src/App.vue
+++ blog/src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+  <HomeMy/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HomeMy,
   }
 }
 </script>
